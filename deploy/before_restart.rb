750602898237ee6fecdcc44c3e9928e45309b37d
mpd <mpd@stipple.com>
2013-11-04 13:54:47 -0800

    use npm-installed coffee-script version

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,3 @@
-`/usr/local/bin/coffee -c #{release_path}/server.coffee`
+nodejs_version = `/usr/bin/env node -v`.chomp.sub(/^v?/, '')
+`/opt/nodejs/#{nodejs_version}/bin/coffee -c #{release_path}/server.coffee`
 `sudo monit restart camo`
